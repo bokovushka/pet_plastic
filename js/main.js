58302f193fe0aab0b1f2561756832c966f1f47eb
bokovushka <57456193+bokovushka@users.noreply.github.com>
2022-07-09 16:36:34 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -18,7 +18,12 @@ $(document).ready(function () {
 		body_lock.classList.remove('lock');
 	})
 });
-
+$('.nav-menu .anchor').click(function () {
+	$('.menu-btn').removeClass("active");
+	$('.menu-btn-m').removeClass("active");
+	$('.m-wrap').removeClass("active");
+	$('body').removeClass("lock");
+});
 
 // tabs
 $('.tab-view li').click(function () {
@@ -33,3 +38,19 @@ jQuery(function ($) {
 	$("#tel").mask("+380(99) 999-99-99");
 });
 
+//href link
+$(document).ready(function () {
+	$('.anchor').bind("click", function (e) {
+		var anchor = $(this);
+		$("html, body")
+			.stop()
+			.animate(
+				{
+					scrollTop: $(anchor.attr("href")).offset().top - 40,
+				},
+				800
+			);
+		e.preventDefault();
+	});
+	return false;
+});
